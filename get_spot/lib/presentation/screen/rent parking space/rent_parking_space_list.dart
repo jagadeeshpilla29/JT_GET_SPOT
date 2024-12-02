8cd8d8702dbb30151a1083009cc1182693233def
jagadeesh pilla <juvarya@jagadeeshs-MacBook-Air.local>
2024-12-02 15:43:53 +0530

    card fix

--- get_spot/lib/presentation/screen/rent parking space/rent_parking_space_list.dart
+++ get_spot/lib/presentation/screen/rent parking space/rent_parking_space_list.dart
@@ -8,14 +8,25 @@ class RentParkingSpaceScreen extends StatefulWidget {
 }
 
 class _RentParkingSpaceScreenState extends State<RentParkingSpaceScreen> {
-    final Map<String, dynamic> parkingSpace = {
-    'name': 'Downtown Garage',
-    'location': '47 W 13th St, New York',
-    'status': 'Available',
-    'price': 10000,
-    'image': 'assets/images/garage1.png',
-    'isFavorited': false,
-  };
+  final List<Map<String, dynamic>> parkingSpaces = [
+    {
+      'name': 'Downtown Garage',
+      'location': '47 W 13th St, New York',
+      'status': 'Available',
+      'price': 10000,
+      'image': 'assets/images/garage1.png',
+      'isFavorited': false,
+    },
+    {
+      'name': 'Uptown Parking',
+      'location': '123 Main St, New York',
+      'status': 'Occupied',
+      'price': 12000,
+      'image': 'assets/images/garage1.png',
+      'isFavorited': true,
+    },
+    // Add more parking space data here
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -66,86 +77,99 @@ class _RentParkingSpaceScreenState extends State<RentParkingSpaceScreen> {
                 child: Text('List', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: AppColor.Black)),
               ),
             ),
-            SizedBox(height: 16),
-            Card(
-              color: AppColor.White,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-              child: SizedBox(
-                height: 138,
-                width: MediaQuery.of(context).size.width * 0.85,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(borderRadius: BorderRadius.only(topLeft: Radius.circular(15), bottomLeft: Radius.circular(15)), child: Image.asset(parkingSpace['image'], fit: BoxFit.cover)),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(child: Text(parkingSpace['name'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14), overflow: TextOverflow.ellipsis)),
-                                IconButton(
-                                  icon: Icon(parkingSpace['isFavorited'] ? Icons.favorite : Icons.favorite_border, color: parkingSpace['isFavorited'] ? Colors.red : Colors.grey, size: 16),
-                                  onPressed: () {
-                                    setState(() {
-                                      parkingSpace['isFavorited'] = !parkingSpace['isFavorited'];
-                                    });
-                                  },
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Image.asset(RENTLOCATION, height: 16, width: 16, fit: BoxFit.contain),
-                                SizedBox(width: 4),
-                                Expanded(child: Text(parkingSpace['location'], style: TextStyle(color: AppColor.Black, fontSize: 12), overflow: TextOverflow.ellipsis)),
-                              ],
-                            ),
-                            SizedBox(height: 8),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(parkingSpace['status'], style: const TextStyle(fontSize: 12), overflow: TextOverflow.ellipsis),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 16.0),
-                                  child: RichText(
-                                    text: TextSpan(
-                                      text: '\$',
-                                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: AppColor.PrimaryColor),
-                                      children: [TextSpan(text: '${parkingSpace['price']}', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: AppColor.PrimaryColor))],
-                                    ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: parkingSpaces.length,
+                itemBuilder: (context, index) {
+                  final parkingSpace = parkingSpaces[index];
+                  
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                    color: AppColor.White,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    child: 
+                    SizedBox(
+                      height: 138,
+
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.only(topLeft: Radius.circular(15), bottomLeft: Radius.circular(15)),
+                              child: Image.asset(parkingSpace['image'], fit: BoxFit.cover)),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(child: Text(parkingSpace['name'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14), overflow: TextOverflow.ellipsis)),
+                                      IconButton(
+                                        icon: Icon(parkingSpace['isFavorited'] ? Icons.favorite : Icons.favorite_border, color: parkingSpace['isFavorited'] ? Colors.red : Colors.grey, size: 16),
+                                        onPressed: () {
+                                          setState(() {
+                                            parkingSpace['isFavorited'] = !parkingSpace['isFavorited'];
+                                          });
+                                        },
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  child: Text('Book Visit', style: TextStyle(fontSize: 10)),
-                                  style: ElevatedButton.styleFrom(backgroundColor: AppColor.PrimaryColor, foregroundColor: AppColor.Black, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)), minimumSize: Size(82, 32), padding: EdgeInsets.zero),
-                                ),
-                                SizedBox(
-                                  width: 101,
-                                  height: 32,
-                                  child: TextButton(
-                                    onPressed: () {},
-                                    style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                                    child: Text('View Contact', style: TextStyle(color: AppColor.PrimaryColor, fontSize: 10)),
+                                  Row(
+                                    children: [
+                                      Image.asset(RENTLOCATION, height: 16, width: 16, fit: BoxFit.contain),
+                                      SizedBox(width: 4),
+                                      Expanded(child: Text(parkingSpace['location'], style: TextStyle(color: AppColor.Black, fontSize: 12), overflow: TextOverflow.ellipsis)),
+                                    ],
                                   ),
-                                ),
-                              ],
+                                  SizedBox(height: 8),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(parkingSpace['status'], style: const TextStyle(fontSize: 12), overflow: TextOverflow.ellipsis),
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 16.0),
+                                        child: RichText(
+                                          text: TextSpan(
+                                            text: '\$',
+                                            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: AppColor.PrimaryColor),
+                                            children: [TextSpan(text: '${parkingSpace['price']}', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: AppColor.PrimaryColor))],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        child: Text('Book Visit', style: TextStyle(fontSize: 10)),
+                                        style: ElevatedButton.styleFrom(backgroundColor: AppColor.PrimaryColor, foregroundColor: AppColor.Black, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)), minimumSize: Size(82, 32), padding: EdgeInsets.zero),
+                                      ),
+                                      SizedBox(
+                                        width: 101,
+                                        height: 32,
+                                        child: TextButton(
+                                          onPressed: () {},
+                                          style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                                          child: Text('View Contact', style: TextStyle(color: AppColor.PrimaryColor, fontSize: 10)),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           ],
